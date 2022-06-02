c6c16c31b677a1e1de3b4058e88317271d57970b
louisferrysp <louisferrysp@gmail.com>
2022-06-02 21:38:15 +0700

    no uses update

---
+++ Source_code/lms/lib/screen/regis_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegisScreen extends StatefulWidget {
+  const RegisScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RegisScreen> createState() => _RegisScreenState();
+}
+
+class _RegisScreenState extends State<RegisScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
