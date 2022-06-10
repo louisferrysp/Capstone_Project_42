a9463e53c612ed6c500a4e9989a71362bb4771f5
louisferrysp <louisferrysp@gmail.com>
2022-06-10 18:26:45 +0700

    no update

---
+++ Source_code/lms/lib/screen/dashboard_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:lms/screen/dashboard_home_screen.dart';
+
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  int _curIndex = 0;
+  final List<Widget> _widgetOption = <Widget>[
+    const DashBoardHomeScreen(),
+    Text('INI Course'),
+    Text('INI PROFILE'),
+  ];
+
+  void _selectItem(int index) {
+    setState(() {
+      _curIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(),
+      body: _widgetOption.elementAt(_curIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.grey,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_sharp, color: Colors.black),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_book_outlined, color: Colors.black),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.supervised_user_circle, color: Colors.black),
+            label: '',
+          ),
+        ],
+        currentIndex: _curIndex,
+        selectedItemColor: Colors.white,
+        onTap: _selectItem,
+      ),
+    );
+  }
+}
