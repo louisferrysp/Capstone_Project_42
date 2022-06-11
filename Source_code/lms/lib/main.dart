e597d37cc427e3ebcac2e9209cedf6aa30e1bda9
louisferrysp <louisferrysp@gmail.com>
2022-06-11 15:54:51 +0700

    add new route

--- Source_code/lms/lib/main.dart
+++ Source_code/lms/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lms/screen/course_screen.dart';
+import 'package:lms/screen/dashboard_course_screen_active.dart';
+import 'package:lms/screen/dashboard_course_screen_past.dart';
 import 'package:lms/screen/dashboard_screen.dart';
 import 'package:lms/screen/login_screen.dart';
 import 'package:lms/screen/regis_screen.dart';
@@ -11,6 +14,10 @@ void main() {
       'regis': (context) => const RegisScreen(),
       'login': (context) => const LoginScreen(),
       'dashboard': (context) => const DashboardScreen(),
+      'dashboard_active_screen': (context) =>
+          const DashBoardCourseActiveScreen(),
+      'dashboard_past_screen': (context) => const DashBoardCoursePastScreen(),
+      'course_screen': (context) => CourseScreen(),
     },
   ));
 }
