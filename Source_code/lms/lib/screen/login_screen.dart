d035634061237b32ce85cdbc61b5798e178825fb
louisferrysp <louisferrysp@gmail.com>
2022-06-13 13:32:03 +0700

    Update

--- Source_code/lms/lib/screen/login_screen.dart
+++ Source_code/lms/lib/screen/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:lms/screen/regis_screen.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -12,105 +13,156 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(25),
-        child: Center(
-          child: ListView(
-            children: [
-              Column(
-                children: [
-                  const Text(
-                    'Login',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  ),
-                  const SizedBox(height: 20),
-// ================================================================================================ //
-//                FORM EMAIL                                                                        //
-// ================================================================================================ //
-                  TextFormField(
-                    controller: emailPerusahaan,
-                    decoration: const InputDecoration(
-                      labelText: 'Alamat Email',
-                      contentPadding:
-                          EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10),
-                        ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  const Text('Gunakan Email Yang Telah Terdaftar'),
-                  const SizedBox(height: 10),
-// ================================================================================================ //
-//                FORM PASSWORD                                                                     //
-// ================================================================================================ //
-                  TextFormField(
-                    controller: password,
-                    decoration: const InputDecoration(
-                      labelText: 'Kata Sandi',
-                      contentPadding:
-                          EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10),
-                        ),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-// ================================================================================================ //
-//                BUTTON LOGIN                                                                      //
-// ================================================================================================ //
-                  ElevatedButton(
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.grey)),
-                    onPressed: () {
-                      Navigator.pushNamed(context, 'dashboard');
-                    },
-                    child: Text('Login'),
-                  ),
-// ================================================================================================ //
-//                BUTTON BELUM PUNYA AKUN                                                           //
-// ================================================================================================ //
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+      body: Center(
+        child: ListView(
+          children: [
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
                     children: [
-                      const Text('Belum Punya Akun ?'),
-                      const SizedBox(height: 1),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, 'regis');
-                        },
-                        child: const Text(
-                          'Sign Up',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, color: Colors.black),
+                      const Text(
+                        'Login',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      const SizedBox(height: 20),
+                      // ================================================================================================ //
+                      //                FORM EMAIL                                                                        //
+                      // ================================================================================================ //
+                      TextFormField(
+                        controller: emailPerusahaan,
+                        decoration: const InputDecoration(
+                          labelText: 'Alamat Email',
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 15, horizontal: 20),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
                         ),
                       ),
                       const SizedBox(height: 10),
+                      const Text('Gunakan Email Yang Telah Terdaftar'),
+                      const SizedBox(height: 10),
+                      // ================================================================================================ //
+                      //                FORM PASSWORD                                                                     //
+                      // ================================================================================================ //
+                      TextFormField(
+                        controller: password,
+                        decoration: const InputDecoration(
+                          labelText: 'Kata Sandi',
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 15, horizontal: 20),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.grey),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      // ================================================================================================ //
+                      //                BUTTON LOGIN                                                                      //
+                      // ================================================================================================ //
+                      ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.grey)),
+                        onPressed: () {
+                          Navigator.pushNamed(context, 'dashboard');
+                        },
+                        child: Text('Login'),
+                      ),
+                      // ================================================================================================ //
+                      //                BUTTON BELUM PUNYA AKUN                                                           //
+                      // ================================================================================================ //
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text('Belum Punya Akun ?'),
+                          const SizedBox(height: 1),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, 'regis');
+                            },
+                            child: const Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                        ],
+                      ),
+                      const SizedBox(height: 100),
                     ],
                   ),
-                ],
-              ),
-            ],
-          ),
+                ),
+                // ================================================================================================ //
+                //                Kontak kami, Information, FaQ , Tentang Kami                                      //
+                // ================================================================================================ //
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  height: 250,
+                  width: double.infinity,
+                  color: Colors.grey,
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 20),
+                      Container(
+                        color: Colors.white,
+                        height: 50,
+                        width: double.infinity,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            const Text('Kontak Kami'),
+                            SvgPicture.asset('assets/images/instagram.svg'),
+                            SvgPicture.asset('assets/images/facebook.svg'),
+                            SvgPicture.asset('assets/images/telegram.svg'),
+                            SvgPicture.asset('assets/images/youtube.svg'),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      const Align(
+                        alignment: Alignment.topLeft,
+                        child: Text('Informasi'),
+                      ),
+                      const SizedBox(height: 5),
+                      const Align(
+                        alignment: Alignment.topLeft,
+                        child: Text('FAQ'),
+                      ),
+                      const SizedBox(height: 5),
+                      const Align(
+                        alignment: Alignment.topLeft,
+                        child: Text('Tentang Kami'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
