792285375e1ed61afd3b4ec65e4677a9e294a2d7
louisferrysp <louisferrysp@gmail.com>
2022-06-13 13:32:12 +0700

    Update

--- Source_code/lms/lib/screen/dashboard_course_screen_active.dart
+++ Source_code/lms/lib/screen/dashboard_course_screen_active.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class DashBoardCourseActiveScreen extends StatefulWidget {
   const DashBoardCourseActiveScreen({Key? key}) : super(key: key);
@@ -39,7 +40,70 @@ class _DashBoardCourseScreenState extends State<DashBoardCourseActiveScreen> {
                   ),
                 ),
               ],
-            )
+            ),
+            const SizedBox(height: 20),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  height: 250,
+                  width: double.infinity,
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 120,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[700],
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      const Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Beginner UI /UX Design',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Container(
+                        child: const Text(
+                            'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'),
+                      ),
+                      const SizedBox(height: 10),
+                      LinearPercentIndicator(
+                        width: 220,
+                        lineHeight: 20,
+                        percent: 0.5,
+                        center: Text('Progress'),
+                        backgroundColor: Colors.white,
+                        progressColor: Colors.grey,
+                        trailing: Container(
+                          height: 20,
+                          width: 60,
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Center(
+                            child: InkWell(
+                              onTap: () {},
+                              child: const Text(
+                                'Resume',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
